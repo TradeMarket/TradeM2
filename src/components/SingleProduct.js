f013bc10e208fb24a2328442ceca0445a4d69269
David Park <davpark1004@gmail.com>
2023-08-07 17:59:49 -0400

    created singleproduct component and set up the base of the component

---
+++ src/components/SingleProduct.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Image, Carousel } from "react-bootstrap";
+
+export default function SingleProduct() {
+  //make sure to get product images and info from firebase
+  return (
+    <div>
+      <Image src="https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small/default-avatar-profile-icon-of-social-media-user-vector.jpg" roundedCircle />
+      <Carousel>
+        <Carousel.Item>
+          <Image src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" fluid />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image src="https://images.pexels.com/photos/821651/pexels-photo-821651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" fluid/>
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image src="https://images.pexels.com/photos/279906/pexels-photo-279906.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" fluid/>
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
+      <p>
+        Item Description goes here.
+      </p>
+    </div>
+  );
+}
