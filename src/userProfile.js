72c32eb196cf1df8dd369fa2d05f75037dc7819c
David Park <davpark1004@gmail.com>
2023-08-07 16:55:11 -0400

    tried to get data from backend on user profile page

--- src/userProfile.js
+++ src/userProfile.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { getDatabase, ref } from 'firebase/database';
 
+const db = getDatabase();
+//get user from users table, how do I know which user to get
+//I need to get the info of the user that is logged in right now
+const userRef = ref(db, 'Users/' + 1);
 const UserProfile = () => {
   //get the image, name (first name + last name), email, and user name from firebase and replace the placeholders
   return (
@@ -7,13 +12,13 @@ const UserProfile = () => {
       <div className="topProfile">
         <div>
           <img className="userProfilePic"
-          src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/512px-Windows_10_Default_Profile_Picture.svg.png?20221210150350'/>
+          src={userRef['Profile Image']}/>
         </div>
         <div>
-          <h4 className='profileName'>Firstname Lastname</h4>
+          <h4 className='profileName'>{userRef['FirstName']} {userRef['LastName']}</h4>
           <div className='userProfileDetails'>
-            <h6>Email:</h6>
-            <h6>Username:</h6>
+            <h6>Email: {userRef['Email']}</h6>
+            <h6>Username:{userRef['Username']}</h6>
             <h6>Password: ******</h6>
             <h6>Reset Password</h6>
           </div>
