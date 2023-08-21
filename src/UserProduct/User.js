bc7b52e46df6f361017fc479d7e2d526a480c630
Brian Ngo <brianhngo@gmail.com>
2023-08-21 15:04:38 -0700

    hi

--- src/UserProduct/User.js
+++ src/UserProduct/User.js
@@ -75,7 +75,6 @@ export default function User({ uid }) {
       const file = files[i];
       const storage = getStorage();
       const storageRef = refFromStorage(storage, 'productImages/' + file.name);
-      const storageRef = refFromStorage(storage, 'productImages/' + file.name);
       const uploadTask = uploadBytesResumable(storageRef, file);
 
       uploadTask.on(
@@ -100,12 +99,11 @@ export default function User({ uid }) {
             if (uploadedImageUrls.length === files.length) {
               setProductImages(uploadedImageUrls);
             }
-
           } catch (error) {
             console.error('Error getting download URL:', error);
           }
         }
-      )
+      );
     }
   };
 
@@ -158,12 +156,12 @@ export default function User({ uid }) {
 
       setNewProduct({
         productId: id,
-        imageUrl: "",
-        name: "",
-        description: "",
-        price: "",
-        category: "",
-        location: "",
+        imageUrl: '',
+        name: '',
+        description: '',
+        price: '',
+        category: '',
+        location: '',
       });
     } catch (error) {
       console.error('Error adding product', error);
@@ -233,9 +231,15 @@ export default function User({ uid }) {
   return (
     <div className="body">
       <button className="showAddProduct-btn" onClick={toggleAddProduct}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-          <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="30"
+          height="30"
+          fill="currentColor"
+          class="bi bi-plus-circle"
+          viewBox="0 0 16 16">
+          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+          <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
         </svg>
       </button>
       {showAddProduct && (
--- src/components/About.js
+++ src/components/About.js
@@ -1,29 +1,51 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import '../../public/Styles.css'; 
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import '../../public/Styles.css';
 
 export default function About() {
   return (
-    <div className="min-vh-100 d-flex align-items-center justify-content-center bg-black">
-      <Container>
+    <div className="min-vh-100 d-flex align-items-center justify-content-center bg-light">
+      <Container className="mx-auto" style={{ maxWidth: '100%' }}>
         <Row className="justify-content-center">
           <Col xs={12} md={10} lg={8}>
             <div className="text-center">
-              <h1 className="display-4 mt-5 text-white border border-light">
+              <h1
+                className="display-2 mt-5 mb-4 text-dark"
+                style={{ textDecoration: 'underline' }}>
                 About Us
               </h1>
-              <p className="lead mb-5 text-white">
+
+              <img
+                src="Background1.jpg"
+                alt="About Us"
+                className="img-fluid mb-4"
+                style={{ width: '80%', height: '600px' }}
+              />
+
+              <p
+                className="lead mb-5 text-black"
+                style={{
+                  fontSize: '22px',
+                }}>
                 Welcome to our platform! We are dedicated to providing a
                 seamless and secure experience for users to sell their unwanted
                 goods locally. Our mission is to create a sustainable
                 marketplace that fosters community engagement and promotes
                 responsible consumption.
               </p>
-              <div className="info-section">
-                <h2 className="text-white text-start ">Our Vision</h2>
-                <p className="text-white mb-5 text-start">
+              <div className="bg-light rounded p-4 mb-4">
+                <h2
+                  className="text-dark text-start display-4"
+                  style={{ textDecoration: 'underline' }}>
+                  Our Vision
+                </h2>
+                <p
+                  className="text-black mb-0"
+                  style={{
+                    fontSize: '22px',
+                  }}>
                   At our core, we envision a world where people can easily find
                   new homes for their pre-loved items, reducing waste and
                   minimizing the impact on the environment. We believe in the
@@ -33,9 +55,17 @@ export default function About() {
                 </p>
               </div>
 
-              <div className="info-section">
-                <h2 className="text-white text-end">How It All Started</h2>
-                <p className="text-white mb-5 text-end">
+              <div className="bg-light rounded p-4 mb-4">
+                <h2
+                  className="text-dark text-end display-4"
+                  style={{ textDecoration: 'underline' }}>
+                  How It All Started
+                </h2>
+                <p
+                  className="text-black mb-0"
+                  style={{
+                    fontSize: '22px',
+                  }}>
                   Our journey began with a simple idea: to make selling and
                   buying local goods as effortless as possible. We noticed the
                   growing trend of people seeking alternatives to traditional
@@ -45,59 +75,97 @@ export default function About() {
                 </p>
               </div>
 
-              <div className="info-section">
-                <h2 className="text-white text-start">Our Values</h2>
-                <ul className="text-white mb-5 text-start">
-                  <li>
-                    Transparency: We value openness and honesty. Our platform is
-                    built on a foundation of trust, ensuring that every user has
-                    a safe and transparent experience.
+              <div className="bg-light rounded p-4 mb-4">
+                <h2
+                  className="text-dark text-start display-4"
+                  style={{ textDecoration: 'underline' }}>
+                  Our Values
+                </h2>
+
+                <ul className="about-values-list">
+                  <li className="about-values-item">
+                    <span className="about-value-icon">Transparency:</span>
+                    <span className="about-value-content">
+                      We value openness and honesty. Our platform is built on a
+                      foundation of trust, ensuring that every user has a safe
+                      and transparent experience.
+                    </span>
                   </li>
-                  <li>
-                    Community: We are committed to building strong, vibrant
-                    communities. Through our platform, users can connect with
-                    their neighbors, fostering a sense of camaraderie and
-                    support.
+
+                  <li className="about-values-item">
+                    <span className="about-value-icon">Community:</span>
+                    <span className="about-value-content">
+                      {' '}
+                      We are committed to building strong, vibrant communities.
+                      Through our platform, users can connect with their
+                      neighbors, fostering a sense of camaraderie and support.
+                    </span>
                   </li>
-                  <li>
-                    Sustainability: We care deeply about the environment and
-                    want to play our part in reducing waste. By promoting the
-                    reuse of goods, we strive to contribute to a greener future.
+                  <li className="about-values-item">
+                    <span className="about-value-icon">Sustainability:</span>
+                    <span className="about-value-content">
+                      {' '}
+                      We care deeply about the environment and want to play our
+                      part in reducing waste. By promoting the reuse of goods,
+                      we strive to contribute to a greener future.
+                    </span>
                   </li>
-                  <li>
-                    Convenience: Selling your unwanted items should be
-                    hassle-free. We've designed our platform to be
-                    user-friendly, so you can list your items quickly and
-                    easily.
+                  <li className="about-values-item">
+                    <span className="about-value-icon">Convenience:</span>
+                    <span className="about-value-content">
+                      {' '}
+                      Selling your unwanted items should be hassle-free. We've
+                      designed our platform to be user-friendly, so you can list
+                      your items quickly and easily.
+                    </span>
                   </li>
+                  {/* ... (other list items) ... */}
                 </ul>
               </div>
-              <div className="info-section">
 
-              <h2 className="text-white text-end">
-                Our Promise
-              </h2>
-              <p className="text-white text-end">
-                As we grow, our commitment to our users remains unwavering. We
-                promise to continuously improve and refine our platform, taking
-                into account your valuable feedback. We are here to listen and
-                to ensure that your experience on our platform is nothing short
-                of exceptional.
-              </p>
-              <p className="text-white mb-5 text-end">
-                Join us in our mission to create a thriving marketplace that
-                supports local communities and embraces sustainable practices.
-                Together, we can make a positive impact on the world, one item
-                at a time.
-              </p>
-
-
+              <div className="bg-light rounded p-4 mb-4">
+                <h2
+                  className="text-dark text-end display-4"
+                  style={{ textDecoration: 'underline' }}>
+                  Our Promise
+                </h2>
+                <p
+                  className="text-dark  mb-0"
+                  style={{
+                    fontSize: '18px',
+                  }}>
+                  As we grow, our commitment to our users remains unwavering. We
+                  promise to continuously improve and refine our platform,
+                  taking into account your valuable feedback. We are here to
+                  listen and to ensure that your experience on our platform is
+                  nothing short of exceptional.
+                </p>
+                <p
+                  className="text-dark mb-0"
+                  style={{
+                    fontSize: '18px',
+                  }}>
+                  Join us in our mission to create a thriving marketplace that
+                  supports local communities and embraces sustainable practices.
+                  Together, we can make a positive impact on the world, one item
+                  at a time.
+                </p>
               </div>
-             
-              <p className="text-white">
+
+              <p
+                className="text-black"
+                style={{
+                  fontSize: '18px',
+                }}>
                 Thank you for being a part of our journey.
               </p>
-              <p className="text-white">Team TradeM</p>
+              <p
+                className="text-black"
+                style={{
+                  fontSize: '24px',
+                }}>
+                Team TradeM
+              </p>
             </div>
           </Col>
         </Row>
